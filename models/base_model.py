7b671581a96895931d9f670c0c9aa0ae863728ba
i2513568-ALESTI <i2513568@continental.edu.pe>
2025-08-13 01:13:42 -0500

    arreglando simulador

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 
 class BaseCredito(ABC):
-    def _init_(self, monto, plazo, tasa):
+    def __init__(self, monto, plazo, tasa):
         self.monto = monto
         self.plazo = plazo
         self.tasa = tasa
